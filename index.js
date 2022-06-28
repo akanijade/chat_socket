e48fe3877ad5fe70e4e2918df51e5c03b8ddfe15
Akani Jade <gianiranjani@gmail.com>
2022-06-28 14:09:38 -0700

    start script

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+}); 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
