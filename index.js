62e4ea729cc34e0b075327308d2b2edacceb017b
Akani Jade <gianiranjani@gmail.com>
2022-06-28 13:13:32 -0700

    deploying in heroku

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Hello world</h1>');
+// });
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+// io.on('connection', (socket) => {
+//   console.log('a user connected');
+// });
+// io.on('connection', (socket) => {
+//     console.log('a user connected');
+//     socket.on('disconnect', () => {
+//       console.log('user disconnected');
+//     });
+//   });
+
+// io.on('connection', (socket) => {
+// socket.on('chat message', (msg) => {
+//     console.log('message: ' + msg);
+// });
+// });
+
+// broadcast message to all servers connected to 'io'
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+// server.listen(3000, () => {
+//   console.log('listening on *:3000');
+// });
